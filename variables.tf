23c169de50f50a717f68c1c86a250ab848a5c59b
luzmercd <137191340+luzmercd@users.noreply.github.com>
2023-06-22 15:08:37 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,3 @@
 #  description = "Type of EC2 instance to provision"
 #  default     = "t3.nano"
 #}
-# variable "AWS_SECRET_ACCESS_KEY" {
-# description = "AWS Secret Access Key"
-# type        = string
-#}
