b272ac3bedb8f8b25bce860058133caef679be9a
luzmercd <137191340+luzmercd@users.noreply.github.com>
2023-06-22 14:47:28 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,8 +6,6 @@ terraform {
   }
 }
 
-provider "aws" {}
-$ export AWS_ACCESS_KEY_ID="anaccesskey"
-$ export AWS_SECRET_ACCESS_KEY="asecretkey"
-$ export AWS_REGION="us-west-2"
-$ terraform plan
+provider "aws" {
+  region  = "us-west-2"
+}
